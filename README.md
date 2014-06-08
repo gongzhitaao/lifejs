e7cd6add4731c1ce43a9141511f1c271996a2792
Zhitao Gong <zhitaao.gong@gmail.com>
2014-06-08 18:33:10 -0500

    visualization

---
+++ README.md
@@ -0,0 +1,7 @@
+Game of Life
+============
+
+Yet another visualization of **WRAPPED**
+[Game of Life](https://gongzhitaao.github.io/lifejs/).
+
+Have fun!
--- js/main.js
+++ js/main.js
@@ -1,74 +1,164 @@
 (function($) {
 
-  var width = 960;
-  var height = 500;
-  var live = "#222";
-  var dead = "#eee";
+  var Live = "#222";
+  var Dead = "#eee";
+  var MaxSize = 80;
+  var Edge = 10;
 
   var pat = {
-    data: "bo$2bo$3o!"
+    data: "bo$2bo$3o!",
+    rule: "23/3"
   };
 
   var param = {
     width: 32,
     height: 16,
-    startx: 3,
-    starty: 2
+    startx: Edge,
+    starty: Edge
   };
 
-  var size = 30;
-  var offx = (width - param.width * size) / 2;
-  var offy = (height - param.height * size) / 2;
+  var width, height, offx, offy;
+  var size = MaxSize;
+  var grid = 2;
+  var ctx, gol, next;
+  var tickid;
 
-  var canvas, ctx, gol, next;
+  function UrlParam(url) {
+
+    "use strict";
+
+    var rNull = /^\s*$/,
+        rBool = /^(true|false)$/i;
+    function conv(val) {
+      if (rNull.test(val)) return null;
+      if (rBool.test(val)) return val.toLowerCase() === "true";
+      if (isFinite(val)) return parseFloat(val);
+      if (isFinite(Date.parse(val))) return new Date(val);
+      return val;
+    }
+
+    if (!url) return null;
+
+    return url.split("?")[1].split("&")
+      .reduce(function(acc, cur) {
+        var pair = cur.split("=");
+        acc[unescape(pair[0])] = pair.length > 1
+          ? conv(unescape(pair[1]))
+          : null;
+        return acc;
+      }, {});
+  }
+
+  function clearCanvas(ctx) {
+    ctx.clearRect(0, 0, width, height);
+
+    var s1 = width / param.width;
+    var s2 = height / param.height;
+
+    var a, b;
+    if (s1 < s2) a = s1, b = s2;
+    else a = s2, b = s1;
+
+    if (a < MaxSize) size = Math.floor(a);
+    else size = MaxSize;
+
+    offx = Math.floor((width - param.width * size) / 2);
+    offy = Math.floor((height - param.height * size) / 2);
+
+    grid = Math.floor(size / 15);
+
+    var i, j;
+    ctx.fillStyle = Dead;
+    for (i = 0; i < param.height; ++i) {
+      for (j = 0; j < param.width; ++j) {
+        ctx.fillRect(offx + size * j + grid, offy + size * i + grid,
+                     size - 2 * grid, size - 2 * grid);
+      }
+    }
+  }
+
+  function draw() {
+    var i, xy;
+
+    ctx.fillStyle = Live;
+    for (i = 0; i < next.born.length; ++i) {
+      xy = next.born[i];
+      ctx.fillRect(offx + size * xy[0] + grid, offy + size * xy[1] + grid,
+                   size - 2 * grid, size - 2 * grid);
+    }
+
+    ctx.fillStyle = Dead;
+    for (i = 0; i < next.dead.length; ++i) {
+      xy = next.dead[i];
+      ctx.fillRect(offx + size * xy[0] + grid, offy + size * xy[1] + grid,
+                   size - 2 * grid, size - 2 * grid);
+    }
+
+    next = gol.next();
+  }
+
+  function tick() {
+    draw();
+
+    if (next.dead.length > 0 || next.born.length > 0)
+      tickid = window.setTimeout(tick, 500);
+  }
+
+  function getInfo() {
+    $("input[name=data]").val(pat.data);
+    $("input[name=rule]").val(pat.rule ? pat.rule : "23/3");
+    $("input[name=width]").val(param.width - 2 * Edge);
+    $("input[name=height]").val(param.height - 2 * Edge);
+  }
 
   $(document).ready(function() {
 
-    $('pre code').each(function(i, e) {hljs.highlightBlock(e);});
+    width = $(document).width();
+    height = $(document).height();
 
-    canvas = d3.select("body")
+    var canvas = d3.select("body")
       .insert("canvas", ":first-child")
       .attr("width", width)
       .attr("height", height);
     ctx = canvas.node().getContext("2d");
 
+    $(".toggle").click(function() {
+      $(".control").slideToggle();
+      $(".toggle").toggleClass("open");
+
+      if ($(".toggle").hasClass("open")) {
+        getInfo();
+        clearTimeout(tickid);
+      } else tick();
+    });
+
+    $(".yes").click(function() {
+      pat.data = $("input[name=data]").val();
+      pat.rule = $("input[name=rule]").val();
+      param.width = parseInt($("input[name=width]").val()) + 2 * Edge;
+      param.height = parseInt($("input[name=height]").val()) + 2 * Edge;
+
+      clearCanvas(ctx);
+      next = gol.reset(pat, param);
+      draw();
+    });
+
+    $(".no").click(function(){
+      getInfo();
+    });
+
+    var urlparam = new UrlParam(window.location.search);
+    if (urlparam.data) pat.data = urlparam.data;
+    if (urlparam.rule) pat.rule = urlparam.rule;
+    if (urlparam.x) param.width = urlparam.x + 2 * Edge;
+    if (urlparam.y) param.height = urlparam.y + 2 * Edge;
+
+    clearCanvas(ctx);
+
     gol = life.wrapped();
     next = gol.reset(pat, param);
 
-    (function init() {
-      var i, j;
-      ctx.fillStyle = dead;
-      for (i = 0; i < param.height; ++i) {
-        for (j = 0; j < param.width; ++j) {
-          ctx.fillRect(offx + size * j + 2, offy + size * i + 2,
-                       size - 4, size - 4);
-        }
-      }
-    })();
-
-    (function tick() {
-      var i, xy;
-
-      ctx.fillStyle = live;
-      for (i = 0; i < next.born.length; ++i) {
-        xy = next.born[i];
-        ctx.fillRect(offx + size * xy[0] + 2, offy + size * xy[1] + 2,
-                     size - 4, size - 4);
-      }
-
-      ctx.fillStyle = dead;
-      for (i = 0; i < next.dead.length; ++i) {
-        xy = next.dead[i];
-        ctx.fillRect(offx + size * xy[0] + 2, offy + size * xy[1] + 2,
-                     size - 4, size - 4);
-      }
-
-      next = gol.next();
-
-      if (next.dead.length > 0 || next.born.length > 0)
-        setTimeout(tick, 500);
-    })();
-
+    tick();
   });
 
 })(jQuery);
